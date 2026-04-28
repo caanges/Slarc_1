4040e47c990309ac7a2b04aa264dba6bf245a501
caanges <christoffer.angestam@gmail.com>
2026-04-28 13:39:55 +0200

    Data generation works and putts the data in yolo format. Visibility functions needs to be fixed as everything is always visible

--- scripts/synthetic_data/Data_generation_test.py
+++ scripts/synthetic_data/Data_generation_test.py
@@ -19,8 +19,8 @@ labeling_path = r"H:\Programmering\dva513\Slarc_1\Data\Data_label\json_data"
 object_data = []
 class_map = {}
 
-img_width = scene.render.resolution_x
-img_height = scene.render.resolution_y
+img_width = int(scene.render.resolution_x * scene.render.resolution_percentage / 100)
+img_height = int(scene.render.resolution_y * scene.render.resolution_percentage / 100)
 
 def disable_collection(collection, state):
     for obj in collection.all_objects:
@@ -33,8 +33,11 @@ def visiblity(obj, camera, scene):
         co_2d = world_to_camera_view(scene, camera, world_coord)
 
         if 0 <= co_2d.x <= 1 and 0 <= co_2d.y <= 1 and co_2d.z > 0:
-            return 2
-    return 0
+            return 2  # visible
+        elif co_2d.z > 0:
+            return 1  # in front but outside frame
+        else:
+            return 0  # behind camera
 
 def map_objects(obj):
     name = obj.name.split('.')[0]
@@ -167,22 +170,31 @@ def Generate_data(num, ugv, key_points, SUN, camera, scene):
         camera.rotation_euler[0] = 0
         camera.rotation_euler[1] = 0
     
-        for i in range(loop_size_r):
+        for j in range(loop_size_r):
             object_data.clear()
+
+            #change the sun on a presett intervall Ex. 1:10
+            if (j % 10) == 0:
+                change_sun(SUN)
+
+            #change the angle and rotation of the camera around the 'UGV'
+            angle = j * 0.5
+            camera.location.x = radius * math.cos(angle)
+            camera.location.y = radius * math.sin(angle)
+            camera.rotation_euler[2] = angle
+
+            bpy.context.view_layer.update()
+
             xmin, xmax, ymin, ymax = get_bbox(ugv, camera, scene)
 
             cx = (xmin + xmax) / 2 / img_width
             cy = (ymin + ymax) / 2 / img_height
             w  = (xmax - xmin) / img_width
             h  = (ymax - ymin) / img_height
-            #change the sun on a presett intervall Ex. 1:10
-            if (i % 10) == 0:
-                change_sun(SUN)
-
 
             key_point_list = []
-            for i in range(15):
-                kp = key_points[i]
+            for k in range(15):
+                kp = key_points[k]
 
                 xmin, xmax, ymin, ymax = get_bbox(kp, camera, scene)
 
@@ -192,12 +204,6 @@ def Generate_data(num, ugv, key_points, SUN, camera, scene):
                 vis = visiblity(kp, camera, scene)
 
                 key_point_list.extend([x,y,vis])
-
-            #change the angle and rotation of the camera around the 'UGV'
-            angle = i * 0.5
-            camera.location.x = radius * math.cos(angle)
-            camera.location.y = radius * math.sin(angle)
-            camera.rotation_euler[2] = angle
 
             # render image and make a path with the neccesary data in the name of each picture
             img_path = os.path.join(output_path, f"img{num}_{num_loop_one:04d}.png")
